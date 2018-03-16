3ee96d4a24e4f86db94c61152594d1a44258aa9e
Rifqie Rusyadi <31301534+rifqierusyadi@users.noreply.github.com>
2018-03-16 08:18:06 +0800

    update skp online

--- application/modules/auth/views/default.php
+++ application/modules/auth/views/default.php
@@ -17,7 +17,25 @@
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   <![endif]-->
   <!-- Google Font -->
+  <link rel="apple-touch-icon" sizes="57x57" href="<?php echo base_url(); ?>asset/dist/img/icon/apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="<?php echo base_url(); ?>asset/dist/img/icon/apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="<?php echo base_url(); ?>asset/dist/img/icon/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="<?php echo base_url(); ?>asset/dist/img/icon/apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="<?php echo base_url(); ?>asset/dist/img/icon/apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="<?php echo base_url(); ?>asset/dist/img/icon/apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="<?php echo base_url(); ?>asset/dist/img/icon/apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="<?php echo base_url(); ?>asset/dist/img/icon/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php echo base_url(); ?>asset/dist/img/icon/apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192"  href="<?php echo base_url(); ?>asset/dist/img/icon/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="<?php echo base_url(); ?>asset/dist/img/icon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="<?php echo base_url(); ?>asset/dist/img/icon/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?php echo base_url(); ?>asset/dist/img/icon/favicon-16x16.png">
+  <link rel="manifest" href="<?php echo base_url(); ?>asset/dist/img/icon/manifest.json">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="<?php echo base_url(); ?>asset/dist/img/icon/ms-icon-144x144.png">
+  <meta name="theme-color" content="#ffffff">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+  <style>.login-page{background-image: url("<?= base_url('asset/dist/img/bg-3.jpeg'); ?>") !important; background-repeat: no-repeat; background-attachment: fixed; background-position: center; background-size: cover;}.login-box-body{background: rgba(255,255,255, 0.9) !important}</style>
 </head>
 <body class="hold-transition login-page">
 <div class="login-box">
